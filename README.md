d96e17550f2c60a3e91941837f47359cdd155323
4lhc <444lhc@gmail.com>
2015-07-18 12:35:14 +0530

    skel

---
+++ README.md
@@ -0,0 +1,2 @@
+# chemlambda-py
+A python wrapper script for chemlabda-gui [https://github.com/chorasimilarity/chemlambda-gui]
---
+++ chemlambda.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  chemlambda.py
+#
+#  author : Sreejith S
+#  email  : echo $(base64 -d <<< NDQ0bGhjCg==)@gmail.com
+#  date   : Sat 18 Jul 2015 12:34:38 IST
+#  ver    : 
