6839278c11ed88d298b831bb9ff3805ce6a80c91
4lhc <444lhc@gmail.com>
2015-07-18 23:05:46 +0530

    molparser and settings

--- chemlambda.py
+++ chemlambda.py
@@ -8,3 +8,17 @@
 #  email  : echo $(base64 -d <<< NDQ0bGhjCg==)@gmail.com
 #  date   : Sat 18 Jul 2015 12:34:38 IST
 #  ver    : 
+
+from chemlambda import topology
+from chemlambda import molparser as mp
+
+
+rules = topology.graph
+mol_file = 'mol_files/test.mol'
+
+try:
+    with open(mol_file, 'r') as f:
+        st = f.read().splitlines()
+    print(mp.molParser(st, rules))
+except FileNotFoundError:
+        print("File not found")
--- chemlambda/molparser.py
+++ chemlambda/molparser.py
@@ -11,7 +11,6 @@
 
 # Parse mol files based on rules from topology.py
 
-import topology
 
 def addTargets(d, mid):
     """ addTargets(mol_dict, last_mol_id)
@@ -20,7 +19,7 @@ def addTargets(d, mid):
     """
     #create a list of ports
     mol_ids = [ mol for mol in list(d.keys()) if 'name' in d[mol]]
-    #ports that will find mathces
+    #list of ports that will find mathces
     matched_ports = [] 
     for i, mol in enumerate(mol_ids):
         for t_mol in mol_ids[i+1:]:
@@ -50,34 +49,28 @@ def addTargets(d, mid):
 
 
 
-def molParser(mol_file):
-    """ molParser(mol_file)
+def molParser(f, rules):
+    """ molParser( mol_file_split_list, rules)
         Parses mol files line by line based on rules from topology.py
     """
-    rules = topology.graph
     mols = {}
     mol_id = 0
-    try:
-        with open(mol_file, 'r') as f:
-            for line in f:
-                try:
-                    l = line.split()
-                    #l[0] -- main mol type
-                    #rules[l[0]] -- (list) graph specific rules from topology
-                    mols[str(mol_id)] = { 'type': l[0], 'target': [] }
-                    for i, val in enumerate(rules[l[0]]):
-                        mols[str(mol_id)]['target'].append(str(mol_id) + "_" + str(i))
-                        port_id = str(mol_id) + "_" + str(i)
-                        mols[port_id] = { 'type': val,
-                                'name': l[i+1],
-                                'target': []}
+    for line in f:
+        try:
+            l = line.split()
+            #l[0] -- main mol type
+            #rules[l[0]] -- (list) graph specific rules from topology
+            mols[str(mol_id)] = { 'type': l[0], 'target': [] }
+            for i, val in enumerate(rules[l[0]]):
+                mols[str(mol_id)]['target'].append(str(mol_id) + "_" + str(i))
+                port_id = str(mol_id) + "_" + str(i)
+                mols[port_id] = { 'type': val,
+                        'name': l[i+1],
+                        'target': []}
 
-                    mol_id += 1
-                except IndexError:
-                    print("....................IndexError")
-                    pass
-    except FileNotFoundError:
-        print("File not found")
+            mol_id += 1
+        except IndexError:
+            pass
     #return mols
     return  addTargets(mols, mol_id)
 
