df5d7cc151201987f18f842848f90d450baa58f4
4lhc <444lhc@gmail.com>
2015-07-20 20:47:56 +0530

    created methods

--- chemlambda.py
+++ chemlambda.py
@@ -23,23 +23,47 @@ mol_file = 'mol_files/test.mol'
 dict_atoms = {}  #dict of Atom objects { uid: Atom obj }
 dict_ports = {}
 
+def freeInOut():
+    """
+    Create FROUT, FRIN and thier ports
+    """
 
-parse_data = mp.parseMolfile(mol_file)
+def findMatched():
+    """
+    find LP from matched ports
+    """
+    l = list(dict_ports.values())
+    matched_ports = [ (p1.setMatchedport(p2)) 
+            for (i, p1) in enumerate(l) 
+            for p2 in l[i+1:] 
+            if p1.port_name == p2.port_name]
 
-for i, mol in enumerate(parse_data):
-    uid = mol['atom'] + "_" + str(i)
+
+def readMolfile(mol_file):
+    """
+        readMolfile( str or list of lines)
+        Reads the mol file, parses it and populates dict_atoms and dict_ports
+    """
+    parse_data = mp.parseMolfile(mol_file)
+
+    for i, mol in enumerate(parse_data):
+        uid = mol['atom'] + "_" + str(i)
+        
+        atom_list = topology.graph[mol['atom']]
+        port_list = mol['ports']
+        
+        dict_atoms[uid] = atoms.Atom(uid=uid, atom=mol['atom'], lno=mol['lno'])
+        dict_atoms[uid].targets = [] ###???
+
+        for j, (atom, port_name) in enumerate(zip(atom_list, port_list)):
+            puid = uid + "_" + str(j)
+            dict_ports[puid] = atoms.Port( uid=puid, atom=atom,
+                    port_name=port_name, parent_atom=dict_atoms[uid])
+            dict_atoms[uid].targets.append(dict_ports[puid])
+
     
-    atom_list = topology.graph[mol['atom']]
-    port_list = mol['ports']
     
-    dict_atoms[uid] = atoms.Atom(uid=uid, atom=mol['atom'])
-    dict_atoms[uid].targets = [] ###???
-
-    for j, (atom, port_name) in enumerate(zip(atom_list, port_list)):
-        puid = uid + "_" + str(j)
-        dict_ports[puid] = atoms.Port( uid=puid, atom=atom, port_name=port_name)
-        dict_atoms[uid].targets.append(puid)
-    
-
-print(dict_atoms['L_1'].__dict__)
-print(dict_ports['L_1_0'].__dict__)
+readMolfile(mol_file)
+print(dict_atoms['L_0'].__dict__)
+print(dict_ports['L_0_0'].__dict__)
+#print(dict_atoms)
