53f9088ea5aa8d29fa1ab3cc4070a7b62ca8d27f
4lhc <444lhc@gmail.com>
2015-07-21 23:54:36 +0530

    Added allowed moves

--- chemlambda/topology.py
+++ chemlambda/topology.py
@@ -12,7 +12,7 @@
 # A python port of chemlambda-gui by chorasimilarity (Marius Buliga, http://chorasimilarity.wordpress.com/)
 #
 
-#'Topology' of the graphs (ie; rules to read mol files )
+#'Topology' (ie; rules to read mol files ) and allowed 'Moves' for each atom
 # [http://chorasimilarity.github.io/chemlambda-gui/dynamic/moves.html]
 
 #dict containg rules to identify ports for each atom
@@ -29,3 +29,107 @@ graph = { "L": [ "mi", "lo", "ro" ],
         }
 
 
+#Allowed moves for each atom
+moves = { "L": 
+        { 'ro': 
+            { 'A': 
+                { 'Arrow': ['a', 'e'],
+                    'Arrow': ['d', 'b']
+                    },
+
+                'FO':
+                {  'FI': [ 'j', 'i', 'b'],
+                    'L': [ 'k', 'i', 'd'],
+                    'L': [ 'l', 'j', 'e'],
+                    'FOE': [ 'a', 'k', 'l']
+                    },
+
+                'FOE':
+                {  'FI': [ 'j', 'i', 'b'],
+                    'L': [ 'k', 'i', 'd'],
+                    'L': [ 'l', 'j', 'e'],
+                    'FOE': [ 'a', 'k', 'l']
+                    },
+
+                'T':
+                {  'T': ['a'],
+                    'FRIN': [ 'b' ]
+                    }
+                }
+            },
+        "A": 
+        { 'mo':
+            { 'FO':
+                { 'FOE': [ 'a', 'i', 'j' ],
+                    'A': [ 'i', 'k', 'd' ],
+                    'A': [ 'j', 'l', 'e' ],
+                    'FOE': [ 'b', 'k', 'l']
+
+                    },
+                'FOE':
+                { 'FOE': [ 'a', 'i', 'j' ],
+                    'A': [ 'i', 'k', 'd' ],
+                    'A': [ 'j', 'l', 'e' ],
+                    'FOE': [ 'b', 'k', 'l']
+                    },
+                'T':
+                { 'T': [ 'a'],
+                    'T': ['b']
+
+                    }
+                }
+            },
+        "FI":
+        { 'mo':
+                { 'FOE':
+                    { 'Arrow': [ 'a', 'e'],
+                        'Arrow': [ 'b', 'd']
+
+                        },
+                    'FO':
+                    { 'FO': [ 'a', 'i', 'j' ],
+                        'FI': [ 'i', 'k', 'd' ],
+                        'FI': [ 'j', 'l', 'e' ],
+                        'FO': [ 'b', 'k', 'l']
+                        },
+                    'T':
+                    { 'T': [ 'a'],
+                        'T': ['b']
+
+                        }
+
+                    }
+                },
+        "FO":
+            {  'ro':
+                    { 'FOE':
+                        {  'FI': [ 'j', 'i', 'b'],
+                            'FO': [ 'k', 'i', 'd'],
+                            'FO': [ 'l', 'j', 'e'],
+                            'FOE': [ 'a', 'k', 'l']
+                            },
+                        'T':
+                        { 'Arrow': ['a', 'b']
+                            }
+
+                        },
+                    'lo':
+                    { 'T':
+                        { 'Arrow': [ 'a', 'b']
+                            }
+                        }
+                    },
+            "FOE":
+                { 'ro':
+                        { 'T':
+                            { 'Arrow': [ 'a', 'b']
+                                }
+                            },
+                        'lo':
+                        { 'T':
+                            { 'Arrow': [ 'a', 'b']
+                                }
+                            }
+
+                        }
+                }
