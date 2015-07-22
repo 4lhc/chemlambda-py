544f8029e89cda944fabaeabae35ac0391935610
4lhc <444lhc@gmail.com>
2015-07-22 16:44:00 +0530

    Moved dicts and counters to

---
+++ chemlambda/data.py
@@ -0,0 +1,23 @@
+
+class Counter:
+    """
+    Various (unnecessary) Counters
+    """
+    def __init__(self):
+        self.cycle_count = 0
+        self.move_count = 0
+        #self.per_move_count = { }
+
+
+    def update(self):
+        #per atom counter
+        #per port counter
+        print('update')
+        #
+
+class ChemlambdaDicts:
+    """"""
+    def __init__(self):
+        self.dict_atoms = {}
+        self.dict_ports = {}
+        self.dict_moves = {}
