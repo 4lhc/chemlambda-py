dd94c850d59361b236014076684aac8006b9d202
4lhc <444lhc@gmail.com>
2015-07-22 16:38:56 +0530

    moved all functions from chemlambda.py

--- chemlambda/molparser.py
+++ chemlambda/molparser.py
@@ -14,9 +14,12 @@
 
 # Parse mol files based on rules from topology.py
 
-def parse_mol_file(mol_file):
+from chemlambda import topology
+from chemlambda import atoms 
+
+def _parse_mol_file(mol_file):
     """
-    parse_mol_file(mol_file | list_of_line)
+    _parse_mol_file(mol_file | list_of_line)
     if argument is a string it's treated as a file name and file is read
     if argument is a list, it's treated as a list of lines
     return [{ 'atom': 'L', 'ports': [a, b, c]}, ...]
@@ -52,3 +55,71 @@ def parse_mol_file(mol_file):
     return mols
 
 
+def _read_mol_file(mol_file, atom_count = 0):  
+    """
+        read_mol_file( str or list of lines, count)
+        Reads the mol file, parses it and populates dict_atoms and dict_ports
+    """
+    parse_data = _parse_mol_file(mol_file)
+
+    ac = atom_count
+
+    dict_ports = {}
+    dict_atoms = {}
+
+    for i, mol in enumerate(parse_data):
+        uid = mol['atom'] + "_" + str(ac + i)
+        
+        atom_list = topology.graph[mol['atom']]
+        port_list = mol['ports']
+        
+        dict_atoms[uid] = atoms.Atom(uid=uid, atom=mol['atom'], lno=mol['lno'])
+        dict_atoms[uid].targets = [] ###???
+
+        for j, (atom, port_name) in enumerate(zip(atom_list, port_list)):
+            puid = uid + "_" + str(j)
+            dict_ports[puid] = atoms.Port( uid=puid, atom=atom,
+                    port_name=port_name, parent_atom=dict_atoms[uid])
+            dict_atoms[uid].targets.append(dict_ports[puid])
+
+    return ( dict_atoms, dict_ports)
+
+
+def _find_matched(dict_ports):
+    """
+    Run only once. To read moves from mol file.
+    When the cycle starts another Moves method find_moves() will perform this function
+    find LP from matched ports
+    """
+    L = list(dict_ports.values())
+    [atoms.Port._set_matched_port(p1, p2) 
+            for (i, p1) in enumerate(L) 
+            for p2 in L[i+1:] 
+            if p1.port_name == p2.port_name]
+
+
+
+def _add_frin_frout(dict_atoms, dict_ports):
+    """
+    Create FROUT, FRIN and their ports
+    """
+    L = list(dict_ports.values())
+    i = list(dict_atoms.keys()).__len__()
+
+    for p in L:
+        at = ([ "FRIN", "fo" ], [ "FROUT", "fi"])[p._is_out_port()] #tuple[0] or tuple[1]
+        if p.free == 1:
+            #free port exist
+            uid = at[0] +"_" + str(i)
+            puid = uid + "_0"
+            dict_atoms[uid] = atoms.Atom( uid=uid, atom=at[0], targets = [])
+            dict_ports[puid] = atoms.Port( uid=puid, atom=at[1],
+                     parent_atom=dict_atoms[uid], free=0)
+            dict_atoms[uid].targets.append(dict_ports[puid]) #adding targets
+            
+            if p._is_out_port():
+                p._add_target(dict_ports[puid])
+            else:
+                dict_ports[puid]._add_target(p)
+            p.free = 0
+            i += 1
