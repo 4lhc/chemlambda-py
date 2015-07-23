f35eb13cac14305615ff9a0868868ec9a88bfc3e
4lhc <444lhc@gmail.com>
2015-07-24 00:50:36 +0530

    work on it

--- chemlambda/data.py
+++ chemlambda/data.py
@@ -1,10 +1,10 @@
-
+# TODO: combined dict?? call _deflate _inflate -> snapshot list
 class Counter:
     """
     Various (unnecessary) Counters
     """
     def __init__(self):
-        self.cycle_count = 0
+        self.cycle_count = 0 #intial visualisation = cycle 0
         self.total_move_count = 0
         self.atom_count = 0
         self.port_count = 0
@@ -18,9 +18,8 @@ class Counter:
 class ChemlambdaDicts:
     """"""
     def __init__(self):
-        self.mega_atoms_dict = [] #snapshot of atoms everycycle
-        self.mega_ports_dict = [] 
-        self.mega_moves_dict = []
+        self.mega_atoms_list = []  # snapshot of atoms everycycle
+        self.mega_ports_list = []
+        self.moves_list = []
         self.dict_atoms = {}
         self.dict_ports = {}
-        self.dict_moves = {}
