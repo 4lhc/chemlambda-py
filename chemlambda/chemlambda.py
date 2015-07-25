35aec8268d1f85c24a87b78dad972eb90255fcf2
4lhc <444lhc@gmail.com>
2015-07-25 23:35:05 +0530

    generate_cylcle(range)

--- chemlambda/chemlambda.py
+++ chemlambda/chemlambda.py
@@ -59,7 +59,7 @@ def print_dict_ports(dict_ports, title=''):
     print("\033[92;1m{:^70}\033[0m".format(title))
     head = " {:<10} {} {:<10} {} {:<10} {} {:<10} {} {:<10} {} {:>20} {}".format(
             'key', vl, 'uid', vl, 'atom', vl,
-            'parent', vl, 'port_name', vl, 'targets', vl)
+            'parent', vl, 'sources', vl, 'targets', vl)
 
     print(hl*head.__len__())
     print(head)
@@ -70,10 +70,11 @@ def print_dict_ports(dict_ports, title=''):
         atom = d['atom']
         parent = d['parent_atom'].uid
         port_name = d['port_name']
+        sources = ', '.join([p.uid for p in d['sources']])
         targets = ', '.join([p.uid for p in d['targets']])
         print(" {:<10} {} {:<10} {} {:<10} {} {:<10} {} {:<10} {} {:>20} {}"
               .format(k, vl, uid, vl, atom, vl,
-                      parent, vl, port_name, vl, targets, vl))
+                      parent, vl, sources, vl, targets, vl))
 
     print(hl*head.__len__())
 ################################################################################
@@ -89,9 +90,12 @@ def validate_mol_file(mol_file, ignore_errors=True):
 
 def intialise(mol_file):
     """
-    Read mol file and generate initial configuration of atoms ports and their
+    Read mol file and generate initial configuration of atoms, ports and their
     links
     """
+    dicts._reset()
+    counter._reset()
+
     d_a, d_p = mp._read_mol_file(mol_file)
 
     dict_atoms.update(d_a)
@@ -99,22 +103,39 @@ def intialise(mol_file):
 
     mp._find_matched(dict_ports)
     mp._add_frin_frout(dict_atoms, dict_ports)
-    # clear generic port_names
+    # clear original port_names
     moves.Moves._delete_attr(dict_ports, 'port_name')
+
+    print_dict_ports(dict_ports, "Ports")
 
     counter.atom_count = list(dict_atoms.keys()).__len__()
     counter.port_count = list(dict_ports.keys()).__len__()
-
-    print_dict_atoms(dict_atoms, "Atoms")
-    print_dict_ports(dict_ports, "Ports")
+    dicts._take_snapshot(counter.cycle_count)
 
 
-def generate_cycle(max_cycles=50):
+def generate_cycle(*args):
     """
+    generate_cycle([start, [step]], max=50)
     Generate cycles up to max_cycles [default 50] or when all moves are
     exhausted
     """
-    while counter.cycle_count < max_cycles:
+    if len(args) == 3:
+        start, step, max_c = args
+    elif len(args) == 2:
+        start, max_c = args
+        step = 1
+    elif len(args) == 1:
+        max_c, = args
+        start, step = [0, 1]
+    elif len(args) == 0:
+        start, step, max_c = [0, 1, 50]
+    else:
+        start, step, max_c = args[:3]
+
+    # range_list = list(range(start, step, max_c))
+    range_list = [i for i in range(start, max_c, step)]
+
+    while counter.cycle_count < max_c:
         print("{:^70}".format(counter.cycle_count))
         counter.cycle_count += 1
         M = [moves.Moves(a, counter) for a in dict_atoms.values()
@@ -125,26 +146,20 @@ def generate_cycle(max_cycles=50):
         M = [m._del_atoms_ports_from_dict(dict_atoms, dict_ports) for m in M]
         M = [m._add_atoms_ports_to_dict(dict_atoms, dict_ports) for m in M]
 
-        #for m in M:
-            #d_a, d_p = m._atoms_to_add()  # creates new atoms on this call
-            #[dict_atoms.__delitem__(k) for k in m._atoms_to_delete()]
-            #dict_atoms.update(d_a)
-            #[dict_ports.__delitem__(k) for k in m._ports_to_delete()]
-            #dict_ports.update(d_p)
-
-        M = [m._move_snapshot() for m in M]
-        dicts._take_snapshot(counter.cycle_count)
-        dicts.moves_list.append(M)
-
-        print_dict_atoms(dict_atoms, "Atoms")
         print_dict_ports(dict_ports, "Ports")
+
+        if counter.cycle_count in range_list + [max_c]:
+            M = [m._move_snapshot() for m in M]
+            dicts._take_snapshot(counter.cycle_count)
+            dicts.moves_list[counter.cycle_count] = M
+
         # end of one cycle
 
 
 def main():
     intialise('mol_files/small.mol')
     generate_cycle(50)
-    print(dicts.moves_list[0][0].LP_RP)
+    print(dicts.moves_list[1][0].LP_RP)
     return 0
 
 if __name__ == '__main__':
