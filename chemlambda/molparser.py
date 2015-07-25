3ff4316c14d8431b6d0f7df82294c5202b142f56
4lhc <444lhc@gmail.com>
2015-07-25 23:24:07 +0530

    port, new var sources

--- chemlambda/molparser.py
+++ chemlambda/molparser.py
@@ -36,14 +36,14 @@ def _parse_mol_file(mol_file):
             counter = [all_names.count(i) for i in l[1:]]
             if max(counter) > 2 or l[0] not in topology.graph:
                 print("\033[91mError:\033[0m in mol file\nline \033[92m{}\033[0m:{}"
-                        .format(i+1, line))
+                      .format(i+1, line))
             else:
-                mols.append({ "atom": l[0], "ports": l[1:], "lno":i+1})
+                mols.append({"atom": l[0], "ports": l[1:], "lno": i+1})
         except IndexError:
-            #ignore empty lines
+            # ignore empty lines
             pass
         except ValueError:
-            #ignore error raised by max() at empty lines
+            # ignore error raised by max() at empty lines
             pass
     return mols
 
@@ -51,14 +51,14 @@ def _parse_mol_file(mol_file):
 def _read_mol_file(mol_file, atom_count=0):
     """
         read_mol_file( str or list of lines, count)
-        Reads the mol file, parses it and populates dict_atoms and dict_ports
+        Reads the mol file, parses it and populates d_a and d_p
     """
     parse_data = _parse_mol_file(mol_file)
 
     ac = atom_count
 
-    dict_ports = {}
-    dict_atoms = {}
+    d_p = {}
+    d_a = {}
 
     for i, mol in enumerate(parse_data):
         uid = mol['atom'] + "_" + str(ac + i)
@@ -66,51 +66,53 @@ def _read_mol_file(mol_file, atom_count=0):
         atom_list = topology.graph[mol['atom']]
         port_list = mol['ports']
 
-        dict_atoms[uid] = atoms.Atom(uid=uid, atom=mol['atom'], lno=mol['lno'])
-        dict_atoms[uid].targets = []  # ???
+        d_a[uid] = atoms.Atom(uid=uid, atom=mol['atom'], lno=mol['lno'])
+        d_a[uid].targets = []  # ???
 
         for j, (atom, port_name) in enumerate(zip(atom_list, port_list)):
             puid = uid + "_" + str(j)
-            dict_ports[puid] = atoms.Port(uid=puid, atom=atom,
-                                          port_name=port_name,
-                                          parent_atom=dict_atoms[uid])
-            index = dict_atoms[uid]._insert_into(dict_ports[puid])
-            dict_atoms[uid].targets.insert(index, dict_ports[puid])
-    return (dict_atoms, dict_ports)
+            d_p[puid] = atoms.Port(uid=puid, atom=atom,
+                                   port_name=port_name,
+                                   parent_atom=d_a[uid])
+            index = d_a[uid]._insert_into(d_p[puid])
+            d_a[uid].targets.insert(index, d_p[puid])
+    return (d_a, d_p)
 
 
-def _find_matched(dict_ports):
+def _find_matched(d_p):
     """
     """
-    L = list(dict_ports.values())
+    L = list(d_p.values())
     [atoms.Port._set_matched_port(p1, p2)
      for (i, p1) in enumerate(L)
      for p2 in L[i+1:]
      if p1.port_name == p2.port_name]
 
 
-def _add_frin_frout(dict_atoms, dict_ports):
+def _add_frin_frout(d_a, d_p):
     """
     Create FROUT, FRIN and their ports
     """
-    L = list(dict_ports.values())
-    i = list(dict_atoms.keys()).__len__()
+    L = list(d_p.values())
+    i = list(d_a.keys()).__len__()
 
     for p in L:
-        at = (["FRIN", "fo"], ["FROUT", "fi"])[p._is_out_port()]  #tuple[]
+        at = (["FRIN", "fo"], ["FROUT", "fi"])[p._is_out_port()]  # tuple[]
         if p.free == 1:
             # free port exist
             uid = at[0] + "_" + str(i)
             puid = uid + "_0"
-            dict_atoms[uid] = atoms.Atom(uid=uid, atom=at[0], targets=[])
-            dict_ports[puid] = atoms.Port(uid=puid, atom=at[1],
-                                          parent_atom=dict_atoms[uid],
-                                          free=0)
-            dict_atoms[uid].targets.append(dict_ports[puid])  # adding targets
+            d_a[uid] = atoms.Atom(uid=uid, atom=at[0], targets=[])
+            d_p[puid] = atoms.Port(uid=puid, atom=at[1],
+                                   parent_atom=d_a[uid],
+                                   free=0)
+            d_a[uid].targets.append(d_p[puid])  # adding targets
 
             if p._is_out_port():
-                p._add_target(dict_ports[puid])
+                p._add_target(d_p[puid])
+                d_p[puid]._add_source(p)
             else:
-                dict_ports[puid]._add_target(p)
+                d_p[puid]._add_target(p)
+                p._add_source(d_p[puid])
             p.free = 0
             i += 1
