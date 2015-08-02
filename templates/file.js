4479a905669e00b78023b997768c9421f4a0d9b6
4lhc <444lhc@gmail.com>
2015-08-02 23:29:27 +0530

    Temporary templates for testing

---
+++ templates/file.js
@@ -0,0 +1,69 @@
+
+
+//Constants for the SVG
+var width = 500,
+    height = 500;
+
+//Set up the force layout
+var force = d3.layout.force()
+    .friction(.9)
+    .charge(-20)
+    .linkDistance(10)
+	.gravity(.09)
+    .linkStrength(1)
+	//linkDistance( function(d) { return ((d.source.size + d.target.size + 3)/(40*2)); } )
+    .size([width, height]);
+
+//Append a SVG to the body of the html page. Assign this SVG as an object to svg
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+//Read the data from the mis element 
+var mis = document.getElementById('mis').innerHTML;
+graph = JSON.parse(mis);
+
+//Creates the graph data structure out of the json data
+force.nodes(graph.nodes)
+    .links(graph.links)
+    .start();
+
+//Create all the line svgs but without locations yet
+var link = svg.selectAll(".link")
+    .data(graph.links)
+    .enter().append("line")
+    .attr("class", "link")
+    .style("stroke-width", '1px');
+
+//Do the same with the circles for the nodes - no 
+var node = svg.selectAll(".node")
+    .data(graph.nodes)
+    .enter().append("circle")
+    .attr("class", "node")
+    .attr("r", function (d) { return d.size; })
+    .style("fill", function (d) { return d.color; })
+    .call(force.drag);
+
+
+//Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
+force.on("tick", function () {
+    link.attr("x1", function (d) {
+        return d.source.x;
+    })
+        .attr("y1", function (d) {
+        return d.source.y;
+    })
+        .attr("x2", function (d) {
+        return d.target.x;
+    })
+        .attr("y2", function (d) {
+        return d.target.y;
+    });
+
+    node.attr("cx", function (d) {
+        return d.x;
+    })
+        .attr("cy", function (d) {
+        return d.y;
+    });
+});
